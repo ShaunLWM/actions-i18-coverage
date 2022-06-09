0db09e1fda9b043f276943774a482b92181d8642
Shaun <37434846+ShaunLWM@users.noreply.github.com>
2022-06-10 02:59:26 +0800

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
+/* eslint-disable no-console */
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import * as glob from '@actions/glob'
+import {Octokit} from '@octokit/rest'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const contextPullRequest = github.context.payload.pull_request
+    if (!contextPullRequest) {
+      throw new Error(
+        "This action can only be invoked in `pull_request` events. Otherwise the pull request can't be inferred."
+      )
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const repoToken = core.getInput('repo-token')
 
-    core.setOutput('time', new Date().toTimeString())
+    const octokit = new Octokit({
+      auth: repoToken
+    })
+
+    const files = await octokit.pulls.listFiles({
+      ...github.context.repo,
+      pull_number: contextPullRequest.number
+    })
+
+    const changedFilesGlobber = await glob.create(
+      files.data.map(file => file.filename).join('\n')
+    )
+    const changedFiles = await changedFilesGlobber.glob()
+    console.log(changedFiles)
+
+    const patterns = [
+      'src/locales/translations-*.ts',
+      'src/assets/web-translations-*.json'
+    ]
+    const translationsGlobber = await glob.create(patterns.join('\n'))
+    const translationFiles = await translationsGlobber.glob()
+    console.log(translationFiles)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
